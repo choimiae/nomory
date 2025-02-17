82ff3c068208fe5053dcea242cbb0ca1db3bf0ca
choimiae <chlaldo1220@gmail.com>
2025-02-17 15:47:32 +0900

    axios api 인터셉터 수정

--- src/setup/api.ts
+++ src/setup/api.ts
@@ -24,7 +24,30 @@ api.interceptors.request.use (
 api.interceptors.response.use(
 	(response:AxiosResponse): AxiosResponse => response,
 	(error:AxiosError) : Promise<AxiosError> => {
-		console.log('❌ FAIL - API RESPONSE ERROR');
+		let errorMsg = 'API 요청 실패';
+		if(error.response) {
+			const statusCode = error.response.status;
+			switch (statusCode) {
+				case 400:
+					errorMsg = '잘못된 요청입니다.';
+					break;
+				case 404:
+					errorMsg = '요청한 리소스를 찾을 수 없습니다.';
+					break;
+				case 500:
+					errorMsg = '서버 에러가 발생했습니다.';
+					break;
+				default:
+					errorMsg = '알 수 없는 오류가 발생했습니다.';
+			}
+
+			error.message = errorMsg;
+
+		} else if(error.request) {
+			error.message = '서버 응답이 없습니다.';
+		} else {
+			error.message = '알 수 없는 오류가 발생했습니다.';
+		}
 		return Promise.reject(error);
 	}
 )
