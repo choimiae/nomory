b8a33db9e151eab5fb08a9b7436783120b5a95a3
choimiae <chlaldo1220@gmail.com>
2025-06-10 15:14:14 +0900

    로딩 수정

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Navigate} from 'react-router-dom';
-import CircularProgress from '@mui/material/CircularProgress';
+import {CircularProgress, Box} from '@mui/material';
 import {useUser} from '../contexts/UserContext';
 
 const PrivateRoute = ({children} : {children: JSX.Element}) => {
 	const { user, loading } = useUser();
 
 	if(loading)
-		return <CircularProgress />;
+		return <Box sx={{position:"fixed", left:0, top:0, width:"100%", height:"100%", zIndex:999, display:"flex", alignItems:"center", justifyContent:"center"}}><CircularProgress /></Box>;
 
 	if(!user)
 		return <Navigate to="/login" replace />
